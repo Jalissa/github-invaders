4e9a83a5c3673a452814b8b63b11f6745d27a5e6
jalissa <jalissa2403@gmail.com>
2017-11-03 22:51:56 -0400

    smoothness on shooting and ship movement

--- index.js
+++ index.js
@@ -1,178 +1,232 @@
 const canvas = document.getElementById('canvas');
-canvas.width = 500 || window.innerWidth;
-canvas.height = 500 || window.innerHeight;
+canvas.width =  900;
+canvas.height = 900;
 
 const context = canvas.getContext('2d');
 
 const WIDTH = 60;
 const HEIGHT = 40;
-const COLOR = 'black';
-const INIT_Y = canvas.height - HEIGHT*2;
-const INIT_X = (canvas.width / 2) - WIDTH;
-const intervals = [];
+const INIT_Y = canvas.height - HEIGHT;
+const INIT_X = canvas.width / 2 - WIDTH;
+const LEFT_ARROW_KEY = 37;
+const RIGHT_ARROW_KEY = 39;
+
+let LEFT = false;
+let RIGHT = false;
+
+let shipMovementAnimation;
 
 const settings = {
-  enemy: {
-    width: 60,
-    height: 40,
-    total: 30,
-    margin: 10
-  },
-  ship: {
+	enemies: {
     width: WIDTH,
     height: HEIGHT,
-    initX: INIT_X,
-    initY: INIT_Y,
-    x: INIT_X,
-    y: INIT_Y
-  },
-  shot: {
-    width: 5,
-    height: 20,
-    movement: 15,
-    initX: INIT_X + (WIDTH /2),
-    initY: INIT_Y
+    margin: 10,
+    total: 20,
+    totalVariety: 3,
+    1: {
+      points: 10
+    },
+    2: {
+      points: 5
+    },
+    3: {
+      points: 2
+    }
+	},
+	ships: {
+		width: WIDTH,
+		height: HEIGHT,
+		x: INIT_X,
+		y: INIT_Y
+	},
+	shots: {
+		width: 5,
+		height: 20,
+		movement: 10,
+		y: INIT_Y - HEIGHT / 2
+	},
+	explosions: {
+		count: 30
+	}
+};
+
+const { ships, shots, enemies, explosions } = settings;
+
+function Ship(x, y, width, height, speed = 2, enemyShipInfo) {
+	this.x = x;
+	this.y = y;
+	this.width = width;
+  this.height = height;
+  this.speed = speed;
+  this.level = enemyShipInfo && enemyShipInfo.level;
+  this.points = enemyShipInfo && enemyShipInfo.points;
+
+}
+Ship.prototype.move = function(left, limitWidth) {
+  const {x, speed, width} = this;
+	if(left){
+    const newX = x - speed;
+    this.x = newX >= 0 ? x - speed : x;
+  } else {
+    const newX = x + speed;
+    this.x = newX <= (limitWidth - width) ? x + speed : x;
   }
+};
+
+function Shot(ship, y, width, height, movement) {
+	this.x = ship.x + ship.width / 2 - width;
+	this.y = y;
+	this.width = width;
+	this.height = height;
+	this.movement = movement;
+}
+Shot.prototype.move = function() {
+	this.y -= this.movement;
+};
+
+const player = new Ship(ships.x, ships.y, ships.width, ships.height);
+
+context.fillRect(player.x, player.y, player.width, player.height);
+
+let renderedEnemies = renderEnemies();
+
+function moveShip() {
+  if(LEFT || RIGHT) {
+    context.clearRect(player.x, player.y, player.width, player.height);
+    player.move(LEFT, canvas.width);
+    context.fillRect(player.x, player.y, player.width, player.height);
+  } 
+  shipMovementAnimation = requestAnimationFrame(moveShip);    
 }
 
-const {ship, shot, enemy} = settings;
-
-let shotInitX = shot.initX;
-function Shot() {
-  this.x = shotInitX;
-  this.y = shot.initY;
-  this.width = shot.width;
-  this.height = shot.height;
-}
-Shot.setX = (x) => shotInitX = x;
-Shot.prototype.move = function(){ this.y -= shot.movement };
-
-
-const { color, initX, initY, width, height } = ship;
-
-context.fillStyle = color;
-context.fillRect(initX, initY, width, height);
-
-let enemies = renderEnemies();
-
-shoot(new Shot());
-const shootingInterval = setInterval(() => {
-  shoot(new Shot());
-  if (!enemies.length) {
-    clearInterval(shootingInterval);
+function onkeyup(event) {
+  if(event.keyCode === LEFT_ARROW_KEY) LEFT = false;
+  if(event.keyCode === RIGHT_ARROW_KEY) RIGHT = false;
+  if(!LEFT && !RIGHT){
+    cancelAnimationFrame(shipMovementAnimation);
+    shipMovementAnimation = undefined;
   }
-}, 1500);
-intervals.push(shootingInterval);
 
-document.addEventListener('keydown', move);
+  shoot(event);
+}
 
-function move(event){
-  switch(event.keyCode) {
-    case 37:
-      moveMainShip(() => {
-        ship.x -= width /2;
-        Shot.setX(ship.x + (width /2));
-      });
-      break;
-    case 39:
-      moveMainShip(() => {
-        ship.x += WIDTH /2;
-        Shot.setX(ship.x + (width /2));
-      });
-      break;
-  }
+function onkeydown(event) {
+  if(event.keyCode === LEFT_ARROW_KEY) LEFT = true;
+  if(event.keyCode === RIGHT_ARROW_KEY) RIGHT = true;
+  if(!shipMovementAnimation) moveShip();
 }
-function moveMainShip(operation){
-  context.clearRect(ship.x, initY, width, height);
-  operation();
-  context.fillRect(ship.x, initY, width, height);
-}
+
+document.addEventListener('keydown', onkeydown);
+document.addEventListener('keyup', onkeyup);
 
 function renderEnemies() {
-  const enemies = [];
-
+  const enemyShips = [];
+	const { width, height, margin, speed, total, totalVariety } = enemies;
   let x = 0;
   let y = 0;
-  const width = enemy.width;
-  const height = enemy.height;
+  let currentVariety = 1;
 
-  for(let i = 0; i < enemy.total; i++) {
-    enemies.push({ x, y, width, height });
-    context.fillRect(x, y, width, height);
-    x += width + enemy.margin;
-    if (x >= canvas.width) {
-      x = 0;
-      y += height + enemy.margin;
+	for (let i = 0; i < total*totalVariety; i++) {
+    const enemy = addEnemy(totalVariety, x, y);
+    const newX = enemy.width + margin + x;
+    x = newX + enemy.width <= canvas.width ? newX : 0;
+    if (x === 0) { 
+      y += enemy.height + margin; 
+    }
+    if(i === total*currentVariety){
+      currentVariety++;
     }
   }
+  
+  function addEnemy(varietyNumber, x, y){
+    const enemy = new Ship(x, y, width, height, speed, 
+        { points: enemies[varietyNumber].points, level: varietyNumber } )
+    enemyShips.push(enemy);
+    context.fillRect(enemy.x, enemy.y, enemy.width, enemy.height);
+    return enemy;
+  }
 
-  return enemies;
+	return enemyShips;
 }
 
-function shoot(shot){
-  const shotInterval = setInterval(() => {
-  
-    context.clearRect(shot.x, shot.y, shot.width, shot.height);
-    shot.move();
-    shot.interval = shotInterval;
-    const {collisions, collidedEnemy} = killShip(shot);
-    if (collisions.length || shot.y < 0) {
-      clearInterval(shot.interval);
-      
-      context.clearRect(shot.x, shot.y, shot.width, shot.height);
-      enemies = enemies.filter(enemy => !(enemy.x === collidedEnemy.x && enemy.y === collidedEnemy.y));
-  
-    } else { 
-      context.fillRect(shot.x, shot.y, shot.width, shot.height);      
-    }
-  }, 700);
+function shoot(event) {
+	if (event.keyCode !== 32) return;
+	const shot = new Shot(player, shots.y, shots.width, shots.height, shots.movement);
+	paintShots(shot);
 }
 
 function killShip(shot) {
-  let collidedEnemy = {};
-  const collisions = enemies.filter(enemy => {
-    const collision = enemy.x < shot.x + shot.width &&
-                      enemy.x + enemy.width > shot.x &&
-                      enemy.y < shot.y + shot.height &&
-                      enemy.height + enemy.y > shot.y;
-    if (collision) {
-      collidedEnemy = Object.assign({}, enemy);
-      context.clearRect(enemy.x, enemy.y, enemy.width, enemy.height);
-      explode({
-        width: enemy.width, height: enemy.height,
-        x: enemy.x, y: enemy.y
-      },
-      { x: enemy.x + enemy.width /2 , y: enemy.y + enemy.height /2 },
-      context);
-    
-    }
-    return collision;
-  });
+	let collidedEnemy = {};
+	const collisions = renderedEnemies.filter(enemy => {
+		const collision =
+			enemy.x < shot.x + shot.width &&
+			enemy.x + enemy.width > shot.x &&
+			enemy.y < shot.y + shot.height &&
+			enemy.height + enemy.y > shot.y;
+		if (collision) {
+			collidedEnemy = Object.assign({}, enemy);
+			context.clearRect(enemy.x, enemy.y, enemy.width, enemy.height);
+			explode(
+				{
+					width: enemy.width,
+					height: enemy.height,
+					x: enemy.x,
+					y: enemy.y
+				},
+				{
+					x: enemy.x + enemy.width / 2,
+					y: enemy.y + enemy.height / 2
+				},
+				context
+			);
+		}
+		return collision;
+	});
 
-  return { 
-    collisions,
-    collidedEnemy
-  }
+	return {
+		collisions,
+		collidedEnemy
+	};
 }
-
 
 function explode(container, center, context) {
-  const p = new ParticleSystem(
-    container,
-    center, 
-    30, 
-    context);
-
-  paint();
-  function paint() {
-    context.clearRect(container.x - 10, container.y - 10, container.width + 20, container.height + 20);
-    
-    if(p.update()){
-      context.clearRect(container.x - 10, container.y - 10, container.width + 20, container.height + 20);
-      cancelAnimationFrame(explosionId);
-      return;
-    }
-    var explosionId = requestAnimationFrame(paint);
-  }
+	paintParticles(new ParticleSystem(container, center, explosions.count, context), container);
 }
 
+function paintShots(shot) {
+	let stopMovement = false;
+	// const {x, y, width, height} = shot;
+	const { collisions, collidedEnemy } = killShip(shot);
+	context.clearRect(shot.x, shot.y, shot.width, shot.height);
+
+	if (collisions.length || shot.y < 0) {
+		stopMovement = true;
+		renderedEnemies = renderedEnemies.filter(enemy => !(enemy.x === collidedEnemy.x && enemy.y === collidedEnemy.y));
+		return;
+	} else {
+		shot.move();
+		context.fillRect(shot.x, shot.y, shot.width, shot.height);
+	}
+	const shotMovement = requestAnimationFrame(() => paintShots(shot));
+	if (stopMovement) {
+		cancelAnimationFrame(shotMovement);
+	}
+}
+
+function paintParticles(p, container) {
+	const margin = enemies.margin;
+	const doubleMargin = margin * 2;
+
+	context.clearRect(
+		container.x - margin,
+		container.y - margin,
+		container.width + doubleMargin,
+		container.height + doubleMargin
+	);
+
+	if (p.update()) {
+		cancelAnimationFrame(explosionId);
+		return;
+	}
+	var explosionId = requestAnimationFrame(() => paintParticles(p, container));
+}
--- particle.js
+++ particle.js
@@ -1,65 +1,63 @@
-var Particle = function(x, y, vx, vy) {
+const Particle = function(x, y, vx, vy, width, height) {
 	this.x = x || 0;
 	this.y = y || 0;
 	this.vx = vx || 0;
 	this.vy = vy || 0;
-	
-	this.update = function (vx, vy) {
-		vx = vx || 0,
-		vy = vy || 0;
- 
+	this.width = width || 3;
+	this.height = height || 3;
+
+	this.update = function(vx, vy) {
+		(vx = vx || 0), (vy = vy || 0);
+
 		this.x += this.vx + vx;
 		this.y += this.vy + vy;
 	};
 };
 
-var ParticleSystem = function(container, center, count, context) {
-
+const ParticleSystem = function(container, center, count, context) {
 	count = count || 0;
- 
+
 	this.particles = [];
- 
+
 	this.center = {
 		x: center.x || 0,
 		y: center.y || 0
 	};
- 
-	// Initialization
-	for ( let i = 0; i < count ; ++i ) {
+
+	for (let i = 0; i < count; ++i) {
 		let x = this.center.x,
-				y = this.center.y,
-				vx = Math.random() * 3 - 1.5,
-				vy = Math.random() * 3 - 1.5;
- 
+			y = this.center.y,
+			vx = Math.random() * 3 - 1.5,
+			vy = Math.random() * 3 - 1.5;
+
 		this.particles.push(new Particle(x, y, vx, vy));
 	}
- 
-	this.update = function() {
-		const stopAnimation = this.particles.filter(particle => {
-			return container.x < particle.x + 1 &&
-				container.x + container.width > particle.x &&
-				container.y < particle.y + 1 &&
-				container.y + container.height  > particle.y
-		});
 
-		if(!stopAnimation.length){
+	this.update = function() {
+		const stopAnimation = this.particles.filter(particle => isOutOfContainer(container, particle));
+
+		if (stopAnimation.length < count / 10) {
 			return true;
 		}
-		
-		for (let i = 0 ; i < count ; ++i ) {
-			const x = this.particles[i].x;
-			const y = this.particles[i].y;
-			
-			if (container.x < x + 1 &&
-					container.x + container.width  > x &&
-					container.y < y + 1 &&
-					container.y + container.height  > y) {
 
+		for (let i = 0; i < count; ++i) {
+			const { x, y, width, height } = this.particles[i];
+
+			if (isOutOfContainer(container, this.particles[i])) {
 				this.particles[i].update();
 
-				context.fillRect(x, y, 1, 1);
-			} 
+				context.fillRect(x, y, width, height);
+			}
 		}
-
 	};
+};
+
+const isOutOfContainer = (container, particle) => {
+	const { x, y, width, height } = particle;
+	return (
+		container.x < x + width &&
+		container.x + container.width > x &&
+		container.y < y + height &&
+		container.y + container.height > y
+	);
 };
